35ed3daa4a4447f820b9b49bff972ec05c004bab
Scott Sproule <scott.sproule@ficonab.com>
2010-10-14 10:36:08 +0800

    add email to a list

---
+++ bin/add_to_list.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+# == Synopsis
+#   send mad mimi email 
+# == Usage
+#  add_to_list.rb  -e email -l listname   -k key -u mimiusername 
+# == Author
+#   Scott Sproule  --- Ficonab.com (scott.sproule@ficonab.com)
+# == Example
+# add_to_list.rb  -k xxxx -u xxxx@estormtech.com -l listname -e destination@mail.com
+# == Copyright
+#    Copyright (c) 2010 Ficonab Pte. Ltd.
+#     See license for license details
+require 'yaml'
+require 'rubygems'
+gem 'mad_mimi_two'
+require 'mad_mimi_two'
+require 'optparse'
+require 'rdoc/usage'
+#require 'java' if RUBY_PLATFORM =~ /java/
+# start the processing
+ arg_hash=MadMimiTwo::Options.parse_options(ARGV)
+ RDoc::usage if arg_hash[:help]==true
+require 'pp'
+
+  options = arg_hash
+   # set up variables using hash
+   # note the strange spacing needed to put the ' in the string
+   MadMimiTwo::MadMimiMessage.api_settings = {
+         :username => options[:username].to_s,
+         :api_key => options[:key].to_s
+       }
+  t=MadMimiTwo::MadMimiMessage.new
+  r=t.add_email(options[:list],options[:email])
+  puts "status is #{r} for email #{options[:email]} and list: #{options[:list]}"
